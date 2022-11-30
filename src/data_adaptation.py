e8918d8265a01b73d49114f3140bfbd472bc9c06
Javier Villacampa Porta <115100515+villacampaporta@users.noreply.github.com>
2022-11-30 17:16:01 +0100

    Adaptation 2

--- src/data_adaptation.py
+++ src/data_adaptation.py
@@ -72,14 +72,58 @@ schedule_dict = {
         "L-V: 07:30-21:00; S: 08:30-14:45"                  : "L-S",           
         "L-V: 07:30-21:00; S: 09:30-14:30"                  : "L-S",           
         "L-V: 07:30-21:30; S: 08:00-15:00"                  : "L-S",           
-        "L-X: 07:00-23:00; J: 07:00-23:59; V-S: 00:00-23...": "L-S",           
+        "L-X: 07:00-23:00; J: 07:00-23:59; V-S: 00:00-23:59; D: 00:00-23:00": "L-D",           
         "L: 24H"                                            : "L"  ,         
         "S: 08:00-15:00"                                    : "S"          
                 
         }
 
-# for x in range(len(df)):
-#     df['schedule_parsed'][x] = schedule_dict[df['schedule'][x]] 
-    
+  
 # create a new column based on condition
 df['schedule_parsed'] = df['schedule'].map(schedule_dict)
+
+# Because we have selected only oil stations in Madrid, the columns about 
+# municipality, region, province, town are reduntant
+
+df = df.drop(columns=['province_name', 
+                      'region_name',
+                      'municipality_id',
+                      'municipality_name',
+                      'province_id',
+                      'region_id',
+                      'town'])
+
+#We separate the dataset depending on the oil availability in order to 
+# train the different models.
+
+products = ["gasoline_95E5", 
+            "gasoline_95E5_premium ", 
+            "gasoline_98E5",
+            "gasoline_98E10",
+            "diesel_A",
+            "diesel_B",
+            "diesel_premium",
+            "bioetanol",
+            "biodiesel",
+            "lpg",
+            "cng",
+            "lng",
+            "hydrogen"]
+
+df_gasoline_95E5 = df.where(df['gasoline_95E5']!=0)
+df_gasoline_95E5_premium = df.where(df['gasoline_95E5_premium']!=0)
+df_gasoline_98E5 = df.where(df['gasoline_98E5']!=0)
+df_gasoline_98E10 = df.where(df['gasoline_98E10']!=0)
+df_diesel_A = df.where(df['diesel_A']!=0)
+df_diesel_B = df.where(df['diesel_B']!=0)
+df_diesel_premium = df.where(df['diesel_premium']!=0)
+df_bioetanol = df.where(df['bioetanol']!=0)
+df_biodiesel = df.where(df['biodiesel']!=0)
+df_lpg = df.where(df['lpg']!=0)
+df_cng = df.where(df['cng']!=0)
+df_lng = df.where(df['lng']!=0)
+df_hydrogen = df.where(df['hydrogen']!=0)
+
+
+
+
--- src/dataset.py
+++ src/dataset.py
@@ -7,43 +7,41 @@
 df_lineas = df.copy()
 #df_mapa = df_mapa.sort_values(['station_id', 'date'], ascending=False).groupby('station_id', as_index=False).first()
 
-print(df_lineas)
+#print(df_lineas)
 #print(df_lineas.groupby(['municipality_name', 'station_id']).count())
 
 #No consigo que el product funcione con la tupla mun, station. Querria hacer el trio num, station, fecha
 #mun_stations=df_lineas[['municipality_name', 'station_id']].drop_duplicates().apply(list)
-stations=df_lineas['station_id'].unique()
-date=df_lineas['date'].unique()
+stations = df_lineas['station_id'].unique()
+date = df_lineas['date'].unique()
 sta_dates = pd.DataFrame(list(product(stations, date)),
                      columns=['station_id', 'date'])
 
 #sta_dates = df_lineas.reindex(sta_dates, fill_value=0)
 sta_dates = sta_dates.reindex(columns=df_lineas.columns)
 
-df_lineas=df_lineas.set_index(['station_id', 'date'])
-sta_dates=sta_dates.set_index(['station_id', 'date'])
-sta_dates=sta_dates.drop(df_lineas.index)
+df_lineas = df_lineas.set_index(['station_id', 'date'])
+sta_dates = sta_dates.set_index(['station_id', 'date'])
+sta_dates = sta_dates.drop(df_lineas.index)
 
-
-
-print("Lineas")
-print(df_lineas)
-print("Fechas")
-print(sta_dates)
+# print("Lineas")
+# print(df_lineas)
+# print("Fechas")
+# print(sta_dates)
 
 df_lineas = pd.concat([df_lineas, sta_dates]).sort_index()
 df_lineas = df_lineas.reset_index()
 
 
-print(1)
+# print(1)
 cols = list(df_lineas.columns)
 df_lineas[cols] = df_lineas.groupby('station_id')[cols].bfill()
 df_lineas[cols] = df_lineas.groupby('station_id')[cols].ffill()
 
-print(2)
-print(df_lineas)
-print(3)
-print(df_lineas.groupby(['municipality_name', 'station_id']).count())
+# print(2)
+# print(df_lineas)
+# print(3)
+# print(df_lineas.groupby(['municipality_name', 'station_id']).count())
 
 df = df_lineas
 #df_lineas.to_csv('df_lineas.csv')
